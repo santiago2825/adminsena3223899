1c6477799338a23bc9927b0e96618e06bbc189b3
santiago2825 <quisoboniandres14@gmai.com>
2026-06-26 09:17:53 -0500

    actualizacion de formularios 2.0

--- resources/views/course/create.blade.php
+++ resources/views/course/create.blade.php
@@ -1,48 +1,52 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>course</title>
+    <title>Document</title>
 </head>
 <body>
     <form action="{{route('course.store')}}" method="POST" enctype="multipart/form-data">
-        @csrf 
 
-        <label>
-            numero del curso:
-            <br>
-            <input type="number" name="course_number">
-        </label>
-        <br>
-        <label>
-            dia:
-            <br>
-            <input type="date" name="day">
-        </label>
-        <br>
-        <label for="training_center_id">id centro fromacion</label>
-        <select name="training_center_id" id="training_center_id" class="form-control">
-            <option value="">seleccione un centro de formacion</option>
-            @foreach($training_centers as $training_center)
-            <option value="{{$training_center->id}}">
-                {{$training_center->number}}
-            </option>
+    @csrf
+
+    <label>
+        Numero de Curso: 
+        <input type="number" name="course_number">
+    </label>
+    <br><br>
+    <label>
+        day: 
+        <input type="date" name="day">
+    </label>
+    <br><br>
+
+    <label for="area_id">Area: </label>
+        <select name="area_id" id="area_id" class="form-control">
+            <option value="">Selecione area</option>
+            @foreach($areas as $area)
+                <option value="{{$area->id}}">
+                    {{$area->name}}
+                </option>
             @endforeach
+
+        </select>
+
+        <br><br>
+    <label for="training_center_id">Centro de Formacion: </label>
+        <select name="training_center_id" id="training_center_id" class="form-control">
+            <option value="">Selecione un centro de formacion</option>
+            @foreach($training_centers as $training_center)
+                <option value="{{$training_center->id}}">
+                    {{$training_center->name}}
+                </option>
+            @endforeach
+
         </select>
         <br><br>
-        <label for="area_id">id centro fromacion</label>
-        <select name="area_id" id="area_id" class="form-control">
-            <option value="">seleccione un centro de formacion</option>
-            @foreach($areas as $area)
-            <option value="{{$area->id}}">
-                {{$area->number}}
-            </option>
-            @endforeach
-        </select>
+        
 
 
-        <button type="submit">Enviar</button>
+    <button type="submit">Enviar Formulario</button>
     </form>
 </body>
 </html>
--- resources/views/teacher/create.blade.php
+++ resources/views/teacher/create.blade.php
@@ -1,48 +1,53 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>teacher</title>
+    <title>Document</title>
 </head>
 <body>
     <form action="{{route('teacher.store')}}" method="POST" enctype="multipart/form-data">
-        @csrf 
 
-        <label>
-            nombre:
-            <br>
-            <input type="text" name="name">
-        </label>
-        <br>
-        <label>
-            email:
-            <br>
-            <input type="email" name="email">
-        </label>
-        <br>
-        <label for="training_center_id">id centro fromacion</label>
-        <select name="training_center_id" id="training_center_id" class="form-control">
-            <option value="">seleccione un centro de formacion</option>
-            @foreach($training_centers as $training_center)
-            <option value="{{$training_center->id}}">
-                {{$training_center->number}}
-            </option>
-            @endforeach
-        </select>
-        <br><br>
-        <label for="area_id">area</label>
+    @csrf
+
+    <label>
+        Nombre Instructor: 
+        <input type="text" name="name">
+    </label>
+    <br><br>
+
+    <label>
+        email: 
+        <input type="email" name="email">
+    </label>
+    <br><br>
+
+    <label for="area_id">Area:</label>
         <select name="area_id" id="area_id" class="form-control">
-            <option value="">seleccione un centro de formacion</option>
+            <option value="">Selecione area</option>
             @foreach($areas as $area)
-            <option value="{{$area->id}}">
-                {{$area->number}}
-            </option>
+                <option value="{{$area->id}}">
+                    {{$area->name}}
+                </option>
             @endforeach
+
         </select>
 
+        <br><br>
+    <label for="training_center_id">Centro de Formacion:</label>
+        <select name="training_center_id" id="training_center_id" class="form-control">
+            <option value="">Selecione un centro de formacion</option>
+            @foreach($training_centers as $training_center)
+                <option value="{{$training_center->id}}">
+                    {{$training_center->name}}
+                </option>
+            @endforeach
 
-        <button type="submit">Enviar</button>
+        </select>
+    <br><br>
+        
+
+
+    <button type="submit">Enviar Formulario</button>
     </form>
 </body>
 </html>
